fe3fc72d94ef2aacf64f0941cf8eafc2f19246a3
Ted Mader <tlmader.dev@gmail.com>
2017-01-26 00:42:19 -0600

    Add Photos and Projects components and update router

--- src/router.js
+++ src/router.js
@@ -4,13 +4,16 @@ import { Router, Route, browserHistory } from 'react-router';
 import MainLayout from './components/MainLayout';
 import NavbarInstance from './components/NavbarInstance';
 import Home from './components/Home';
+import Projects from './components/Projects';
+import Photos from './components/Photos';
 
 export default (
   <Router history={browserHistory}>
     <Route component={MainLayout}>
       <Route component={NavbarInstance} />
       <Route path="/" component={Home} />
-      <Route path="photos" component={Home} />
+      <Route path="/projects" component={Projects} />
+      <Route path="/photos" component={Photos} />
     </Route>
   </Router>
 )
