ecba7a74e0e1ea0e32a9e55489a19aa307eaa2ab
Ted Mader <tlmader.dev@gmail.com>
2017-01-26 00:45:15 -0600

    Move view components to views directory

--- src/router.js
+++ src/router.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Router, Route, browserHistory } from 'react-router';
 
-import MainLayout from './components/MainLayout';
 import NavbarInstance from './components/NavbarInstance';
-import Home from './components/Home';
-import Projects from './components/Projects';
-import Photos from './components/Photos';
+import MainLayout from './views/MainLayout';
+import Home from './views/Home';
+import Projects from './views/Projects';
+import Photos from './views/Photos';
 
 export default (
   <Router history={browserHistory}>
--- src/views/MainLayout.js
+++ src/views/MainLayout.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
-import NavbarInstance from './NavbarInstance';
+import NavbarInstance from '../components/NavbarInstance';
 
 class MainLayout extends Component {
   render() {
