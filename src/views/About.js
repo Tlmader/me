f357fa17a9e1e8e17b5db536781b2144009125ac
Ted Mader <tlmader.dev@gmail.com>
2017-01-26 01:25:18 -0600

    Add About view

---
+++ src/views/About.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Photos extends Component {
+  render() {
+    return (
+      <div className="about-page">
+        <h1>About</h1>
+      </div>
+    );
+  }
+}
+
+export default About;
