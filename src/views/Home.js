e6c4c5dbff612ee7e6c446d0dee41bf3c3b81c89
Ted Mader <tlmader.dev@gmail.com>
2017-04-03 17:48:00 -0500

    Remove JumbotronInstance component

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
-import JumbotronInstance from '../components/Jumbotron';
+import { Jumbotron } from 'react-bootstrap';
 
 class Home extends Component {
   render() {
     return (
       <div className="home-page">
-        <JumbotronInstance />
+        <Jumbotron>
+          <h1>Hello, world!</h1>
+          <p>My name is Ted. I live in New Orleans and build software.</p>
+        </Jumbotron>
       </div>
     );
   }
