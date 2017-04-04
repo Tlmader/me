afd0e6c9d9f577386119d553f87867c2be9c1cec
Ted Mader <tlmader.dev@gmail.com>
2017-04-03 21:31:04 -0500

    Add quick about me to Home page and fix attribute names in Music page

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Grid, Col, Panel } from 'react-bootstrap';
 import JumbotronInstance from "../components/Jumbotron";
 
 export default class Home extends Component {
@@ -7,7 +8,12 @@ export default class Home extends Component {
       <div className="home-page">
         <JumbotronInstance
           header="Hello, world!"
-          description="My name is Ted. I live in New Orleans and build software."/>
+          description="My name is Ted. I live in New Orleans and build software!"/>
+        <Grid>
+          <Panel> I study computer science and game development at the University of New Orleans and develop software for
+            GE Digital. PC gaming, electronic music, and men's fashion are my hobbies, and I enjoy discovering places
+            and learning new things.</Panel>
+        </Grid>
       </div>
     );
   }
