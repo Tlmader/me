1014f7e9217c474dc22ac13ec6debe81af8d3f91
Ted Mader <tlmader.dev@gmail.com>
2017-04-03 23:03:39 -0500

    Update Music page to use accordion for containing embedded content

--- src/views/Music.js
+++ src/views/Music.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Grid, Row, Col } from 'react-bootstrap';
+import { Grid, Accordion, Panel } from 'react-bootstrap';
 import JumbotronInstance from "../components/Jumbotron";
 
 export default class Music extends Component {
@@ -10,20 +10,20 @@ export default class Music extends Component {
           header="Music"
           description="Each year's favorite sounds!"/>
         <Grid>
-          <Row>
-            <Col xs={12} md={3}>
+          <Accordion>
+            <Panel header="My 2016 – SoundCloud" eventKey="1" bsStyle="warning">
               <iframe width="100%" height="450" scrolling="no" frameBorder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/297172538&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>
-            </Col>
-            <Col xs={12} md={3}>
+            </Panel>
+            <Panel header="My 2017 – SoundCloud" eventKey="2" bsStyle="warning">
               <iframe width="100%" height="450" scrolling="no" frameBorder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/288248586&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>
-            </Col>
-            <Col xs={12} md={3}>
+            </Panel>
+            <Panel header="Sets & Mixes – SoundCloud" eventKey="3" bsStyle="warning">
               <iframe width="100%" height="450" scrolling="no" frameBorder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/116495333&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>
-            </Col>
-            <Col xs={12} md={3}>
+            </Panel>
+            <Panel header="My 2016 – Spotify" eventKey="4" bsStyle="warning">
               <iframe src="https://embed.spotify.com/?uri=spotify%3Auser%3Atlmader%3Aplaylist%3A3QGFzhkCFDhZhisEDYQvPW" width="300" height="380" frameBorder="0" allowTransparency="true"></iframe>
-            </Col>
-          </Row>
+            </Panel>
+          </Accordion>
         </Grid>
       </div>
     );
