d7fa6f674b14c08b778c879c19ea67f596949322
Ted Mader <tlmader.dev@gmail.com>
2017-04-03 23:18:08 -0500

    Add playlists data and add mapper to accordion

---
+++ src/data/playlists.js
@@ -0,0 +1,18 @@
+export const playlists = [
+  {
+    title: 'My 2016 – SoundCloud',
+    content: '<iframe width="100%" height="450" scrolling="no" frameBorder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/297172538&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>'
+  },
+  {
+    title: 'My 2017 – SoundCloud',
+    content: '<iframe width="100%" height="450" scrolling="no" frameBorder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/288248586&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>',
+  },
+  {
+    title: 'My 2017 – SoundCloud',
+    content: '<iframe width="100%" height="450" scrolling="no" frameBorder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/116495333&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>',
+  },
+  {
+    title: 'My 2016 – Spotify',
+    content: '<iframe src="https://embed.spotify.com/?uri=spotify%3Auser%3Atlmader%3Aplaylist%3A3QGFzhkCFDhZhisEDYQvPW" width="300" height="380" frameBorder="0" allowTransparency="true"></iframe>',
+  },
+];
--- src/views/Music.js
+++ src/views/Music.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Grid, Accordion, Panel } from 'react-bootstrap';
 import JumbotronInstance from "../components/Jumbotron";
+import { playlists } from "../data/playlists";
 
 export default class Music extends Component {
   render() {
@@ -11,18 +12,11 @@ export default class Music extends Component {
           description="Each year's favorite sounds!"/>
         <Grid>
           <Accordion>
-            <Panel header="My 2016 – SoundCloud" eventKey="1" bsStyle="warning">
-              <iframe width="100%" height="450" scrolling="no" frameBorder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/297172538&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>
-            </Panel>
-            <Panel header="My 2017 – SoundCloud" eventKey="2" bsStyle="warning">
-              <iframe width="100%" height="450" scrolling="no" frameBorder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/288248586&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>
-            </Panel>
-            <Panel header="Sets & Mixes – SoundCloud" eventKey="3" bsStyle="warning">
-              <iframe width="100%" height="450" scrolling="no" frameBorder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/116495333&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>
-            </Panel>
-            <Panel header="My 2016 – Spotify" eventKey="4" bsStyle="warning">
-              <iframe src="https://embed.spotify.com/?uri=spotify%3Auser%3Atlmader%3Aplaylist%3A3QGFzhkCFDhZhisEDYQvPW" width="300" height="380" frameBorder="0" allowTransparency="true"></iframe>
-            </Panel>
+            {playlists.map((playlist, i) => (
+              <Panel header={playlist.title} eventKey={i} key={i} bsStyle="warning">
+                <div dangerouslySetInnerHTML={ {__html: playlist.content} } />
+              </Panel>
+            ))}
           </Accordion>
         </Grid>
       </div>
