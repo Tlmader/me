560b176dd926d2f39938442cbf0a9147952e8fc7
Ted Mader <tlmader.dev@gmail.com>
2017-04-03 23:43:21 -0500

    Correct playlist names

--- src/data/playlists.js
+++ src/data/playlists.js
@@ -1,14 +1,14 @@
 export const playlists = [
   {
-    title: 'My 2016 – SoundCloud',
+    title: 'My 2017 – SoundCloud',
     content: '<iframe width="100%" height="450" scrolling="no" frameBorder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/297172538&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>'
   },
   {
-    title: 'My 2017 – SoundCloud',
+    title: 'My 2016 – SoundCloud',
     content: '<iframe width="100%" height="450" scrolling="no" frameBorder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/288248586&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>',
   },
   {
-    title: 'My 2017 – SoundCloud',
+    title: 'Sets & Mixes – SoundCloud',
     content: '<iframe width="100%" height="450" scrolling="no" frameBorder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/116495333&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>',
   },
   {
